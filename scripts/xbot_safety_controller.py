68be8a1c243f883f967942f4101d750b180ffcac
yowlings <825213566@qq.com>
2016-12-28 17:16:46 +0800

    new

---
+++ scripts/xbot_safety_controller.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+#coding=utf-8
+"""
+xbot超声近场测距安全距离控制程序
+
+"""
+
+import rospy,std_msgs.msg
+from geometry_msgs.msg import Twist
+from xbot_msgs.msg import DockInfraRed
+
+class xbot_safety_controller():
+  """docstring for xbot_safety_controller"""
+  def __init__(self):
+    rospy.init_node('xbot_safety_controller')
+    self.pub = rospy.Publisher('/cmd_vel_mux/input/safety_controller', Twist, queue_size = 1)
+    rospy.Subscriber("/sensor/echo_data", DockInfraRed, self.echo_dataCB)
+    rospy.spin()
+
+
+
+  def echo_dataCB(self,echo_data):
+    # print echo_data.center_near
+    if (echo_data.left_near | echo_data.center_near | echo_data.right_near):
+      cmd = Twist()
+      cmd.linear.x = 0
+      cmd.angular.z = 0
+      self.pub.publish(cmd)
+
+
+
+
+if __name__=='__main__':
+ try:
+  rospy.loginfo ("initialization system")
+  xbot_safety_controller()
+  rospy.loginfo ("process done and quit")
+ except rospy.ROSInterruptException:
+  rospy.loginfo("robot twist node terminated.")
